728a431175b49cdbb92033db98b5bc64f91b5fb4
kelvin <km638641@outlook.com>
2023-03-16 08:26:45 -0700

    New line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
  *main - Entry point
  *
- *Description: Prints a message with proper grammar followed by a newline
- *              using printf
+ *Description: Prints a message with proper grammar and a double quote
+ *              followed by the message "but the outcome is a piece of art,",
+ *              followed by a newline, using printf.
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
