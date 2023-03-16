66448def1a8cbfd617cbef103668beb698405dc7
kelvin <km638641@outlook.com>
2023-03-16 05:58:12 -0700

    New world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
+ * Description: Prints a message followed by a newline
  * Return: Always 0 (Success)
  */
 int main(void)
 {
     puts("/"Programming is like building a multilingual puzzle\n");
+
     return (0);
 }
-
