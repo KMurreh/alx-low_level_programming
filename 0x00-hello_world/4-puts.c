da7d88e0b8d05904400227f05adfc9401cca781d
kelvin <km638641@outlook.com>
2023-03-16 06:12:46 -0700

    New world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-    char str{}= "programming is like building a multilingual puzzle";
-    puts(str);
-
+    puts("Programming is like building a multilingual puzzle\n");
     return (0);
 }
