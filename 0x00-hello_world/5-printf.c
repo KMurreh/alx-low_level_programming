bf60ab77b3baa5c0a7f4d394141821f38c0173b2
kelvin <km638641@outlook.com>
2023-03-16 07:53:07 -0700

    New start line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *main -Entry point
+ *main - Entry point
  *
- *Description:Prints a message with proper grammar followed by a newline
- *              using printf
+ *Description: Prints a message with proper grammar and a double quote
+ *              followed by the message "but the outcome is a piece of art,",
+ *              followed by a newline, using printf.
  *
- *Return:Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
