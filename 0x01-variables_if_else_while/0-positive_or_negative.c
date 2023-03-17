067ce7d8895df7a0db69e3ffad0b2b5651b7b60f
kelvin <km638641@outlook.com>
2023-03-17 10:40:49 -0700

    If else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,10 @@
 #include <time.h>
 
 /**
- *main -generates a random number and prints whether it is positive or negative
+ *main - Entry point
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int n;
@@ -15,14 +14,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("Number: %d\n", n);
-
+printf("n = %d\n", n);
 if (n > 0)
+{
 printf("%d is positive\n", n);
+}
 else if (n == 0)
+{
 printf("%d is zero\n", n);
+}
 else
+{
 printf("%d is negative\n", n);
+}
 
 return (0);
 }
