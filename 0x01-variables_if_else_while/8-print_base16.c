24020ade09fba5bd8b1b210aee6fd73cfa423d98
kelvin <km638641@outlook.com>
2023-03-17 13:52:24 -0700

    Single digit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,19 +3,26 @@
 /**
  *main - Entry point
  *
+ *Description: prints all possible combinations of single-digit numbers
+ *Numbers must be separated by ',' followed by a space
+ *Numbers should be printed in ascending order
+ *
  *Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-char c;
+int i;
 
-for (c = '0'; c <= '9'; c++)
-putchar(c);
-
-for (c = 'a'; c <= 'f'; c++)
-putchar(c);
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
 
 putchar('\n');
-
 return (0);
 }
