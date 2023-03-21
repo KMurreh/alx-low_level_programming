5208e458bc7963a1aae0ce3e4728aeb2a6bf2bac
kelvin <km638641@outlook.com>
2023-03-21 02:41:40 -0700

    Putchar only

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,11 @@
-#include <unistd.h>
 #include "main.h"
 
 int main(void)
 {
 char c = '_';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'p';
-write(STDOUT_FILENO, &c, 1);
-c = 'u';
-write(STDOUT_FILENO, &c, 1);
-c = 't';
-write(STDOUT_FILENO, &c, 1);
-c = 'c';
-write(STDOUT_FILENO, &c, 1);
-c = 'h';
-write(STDOUT_FILENO, &c, 1);
-c = 'a';
-write(STDOUT_FILENO, &c, 1);
-c = 'r';
-write(STDOUT_FILENO, &c, 1);
-c = '\n';
-write(STDOUT_FILENO, &c, 1);
-
+write(1, &c, 1), c = 'p', write(1, &c, 1), c = 'u', write(1, &c, 1);
+c = 't', write(1, &c, 1), c = 'c', write(1, &c, 1), c = 'h';
+write(1, &c, 1), c = 'a', write(1, &c, 1), c = 'r', write(1, &c, 1);
+c = '\n', write(1, &c, 1);
 return (0);
 }
