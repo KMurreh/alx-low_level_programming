727354adf436f84c0190d642088d395593785fe2
kelvin <km638641@outlook.com>
2023-03-21 00:46:35 -0700

    Put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <unistd.h>
 
-int main(void) 
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
 {
 char c[] = "_putchar\n";
 int i;
-for (i = 0; c[i] != '\0'; i++) {
+
+for (i = 0; c[i] != '\0'; i++)
+{
 write(1, &c[i], 1);
 }
+
 return (0);
 }
