8dd65310fa54fd892c29eced3006bd086063d7c2
kelvin <km638641@outlook.com>
2023-03-21 02:46:48 -0700

    Putchar only

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 char c = '_';
+
 write(1, &c, 1), c = 'p', write(1, &c, 1), c = 'u', write(1, &c, 1);
 c = 't', write(1, &c, 1), c = 'c', write(1, &c, 1), c = 'h';
 write(1, &c, 1), c = 'a', write(1, &c, 1), c = 'r', write(1, &c, 1);
