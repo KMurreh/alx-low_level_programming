b1d8cac17fdf8384b1ec7bf50eed247dfda6a435
kelvin <km638641@outlook.com>
2023-03-22 01:04:02 -0700

    Reverse digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: the number whose last digit is to be printed
+ *
+ * Return: the value of the last digit
+ */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+int last_digit;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+if (n < 0)
+n = -n;
 
-    printf("%d\n", last_digit);
-
-    return last_digit;
+if (n == 0)
+{
+_putchar('0');
+return (0);
 }
 
-int main()
-{
-    print_last_digit(-123);
-    print_last_digit(0);
-    print_last_digit(456);
-    return 0;
+last_digit = n % 10;
+
+_putchar(last_digit + '0');
+
+return (last_digit);
 }
