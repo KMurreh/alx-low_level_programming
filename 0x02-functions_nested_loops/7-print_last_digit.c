4a14345e891c3741daec58c9956c1d80cb9e24c1
kelvin <km638641@outlook.com>
2023-03-22 01:58:38 -0700

    Int main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  *
  * Return: the value of the last digit
  */
-int print_last_digit(int last_digit)
+int print_last_digit(int n)
 {
 int last_digit;
 
-if (last_digit < 0)
-last_digit = -1;
+if (n < 0)
+n = -n;
 
 last_digit = n % 10;
 
