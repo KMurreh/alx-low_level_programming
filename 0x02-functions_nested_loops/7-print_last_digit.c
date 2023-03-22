ab9681239233f73fa9e59a9dc0eb19e8b92791a4
kelvin <km638641@outlook.com>
2023-03-22 00:32:57 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints the last digit of a number
  *
@@ -9,20 +8,20 @@
  */
 int print_last_digit(int n)
 {
-int last_digit;
+        int last_digit;
 
-if (n < 0)
-n = -n;
+        if (n < 0)
+                n = -n;
 
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
+        if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
 
-last_digit = n % 10;
+        last_digit = n % 10;
 
-_putchar(last_digit + '0');
-
-return (last_digit);
+        _putchar(last_digit + '0');
+	_putchar(int_min + '0');
+        return (last_digit);
 }
