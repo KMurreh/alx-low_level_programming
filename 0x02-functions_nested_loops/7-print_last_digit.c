e9c76d52bc7243ca7bf3ed27f6b3df3e983cc963
kelvin <km638641@outlook.com>
2023-03-22 00:40:01 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -19,9 +19,9 @@ int print_last_digit(int n)
                 return (0);
         }
 
-        last_digit = n % 10;
+        last_digit = (-n % 10)
+		
+	_putchar(last_digit + '0');
 
-        _putchar(last_digit + '0');
-	_putchar(int_min + '0');
         return (last_digit);
 }
