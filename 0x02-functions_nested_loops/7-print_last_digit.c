722b7a70b1802be12d0fc0c48bc74a13f8462812
kelvin <km638641@outlook.com>
2023-03-22 01:51:19 -0700

    Int main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,23 +9,14 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+        int last_digit;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
+        if (n < 0)
+                n = -n;
 
-    if (n < 0)
-        n = -n;
-
-    if (n == INT_MIN)
-        last_digit = 8;
-    else
         last_digit = n % 10;
 
-    _putchar(last_digit + '0');
+        _putchar(last_digit + '0');
 
-    return last_digit;
+        return (last_digit);
 }
