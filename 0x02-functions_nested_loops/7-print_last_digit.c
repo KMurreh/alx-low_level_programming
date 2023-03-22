3e067d57809834d7a1e211e448fd0c801c90863c
kelvin <km638641@outlook.com>
2023-03-22 00:08:21 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *print_last_digit - prints the last digit of a number
+ * print_last_digit - prints the last digit of a number
  *
- *@n: the number whose last digit is to be printed
+ * @n: the number whose last digit is to be printed
  *
- *Return: the value of the last digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
@@ -13,6 +13,12 @@ int last_digit;
 
 if (n < 0)
 n = -n;
+
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
 
 last_digit = n % 10;
 
