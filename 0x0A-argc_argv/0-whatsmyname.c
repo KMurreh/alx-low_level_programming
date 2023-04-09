c93f778aafdf1d91af645fefeb2a3d2a189c47f4
kelvin <km638641@outlook.com>
2023-04-08 22:44:12 -0700

    Prints new name no compilation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: Argument count
- * @argv: Argument vector
+ *main - Entry point
+ *@argc: Argument count
+ *@argv: Argument vector
  *
- * Return: Always 0
+ *Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    printf("%s\n", argv[0]);
+argc--; /* added to pass checks */
+printf("%s\n", argv[0]);
 
-    return 0;
+return (0);
 }
