7ca9089eb58f4adcc4fe78f85e84b7747ed16326
kelvin <km638641@outlook.com>
2023-04-18 01:20:33 -0700

    dog teaches unconditional love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,23 +4,17 @@
 
 /**
  * print_dog - prints a struct dog
- * @d: pointer to struct dog
- *
- * Description: This function prints the contents of a struct dog
- * including the dog's name, age, and owner. If an element of d is NULL,
- * it prints "(nil)" instead of that element.
+ * @d: struct dog to print
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		char *name = d->name != NULL ? d->name : "(nil)";
-		float age = d->age;
-		char *owner = d->owner != NULL ? d->owner : "(nil)";
+	if (d == NULL)
+		return;
 
-		printf("Name: %s\n", name);
-		printf("Age: %.6f\n", age);
-		printf("Owner: %s\n", owner);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-	}	
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
