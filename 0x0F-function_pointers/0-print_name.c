ccf358fe84c41998acb69d31dfebd35b90e36e38
kelvin <km638641@outlook.com>
2023-04-19 02:22:10 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function on each element of an array
- *
- * @array: The array to iterate over
- * @size: The size of the array
- * @action: A pointer to the function to execute
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void print_name(char *name, void (*f)(char *))
 {
-    size_t i;
-
-    if (array == NULL || action == NULL)
-        return;
-
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	if (!name || !f)
+		return;
+	f(name);
 }
