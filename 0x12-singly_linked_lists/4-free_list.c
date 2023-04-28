01328823bb7ec2ffe5ee706dca4f6621fe57d789
kelvin <km638641@outlook.com>
2023-04-27 23:33:17 -0700

    Free lists.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_list - Frees a linked list
+ * @head: Pointer to the first node in the linked list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+    list_t *current_node;
+
+    while (head != NULL)
+    {
+        current_node = head;
+        head = head->next;
+        free(current_node->str);
+        free(current_node);
+    }
+}
