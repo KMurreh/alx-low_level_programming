00d9cc9f5d7ea4bc5154deb28cf1d7fe93bbae95
kelvin <km638641@outlook.com>
2023-04-27 23:42:33 -0700

    100 first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * print_before_main - Prints a message before the main function is executed
+ *
+ * Return: void
+ */
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
