55d8076aa949a814e1ed6210e4d1976a705efa19
kelvin <km638641@outlook.com>
2023-05-05 02:00:01 -0700

    get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * get_endianness - checks the endianness
- *
  * Return: 0 if big endian, 1 if little endian
  */
+
 int get_endianness(void)
 {
-int num = 1;
-char *endian = (char *)&num;
+	int i;
+	char *test;
 
-/* If the first byte is 1, it's little endian */
-if (*endian == 1)
-return 1;
-else
-return 0;
+	i = 1;
+	test = (char *)&i;
+	return ((int)test[0]);
 }
