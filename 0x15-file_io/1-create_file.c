8e991e6b46b8699c4022a94c426a2fb7fa480bf8
kelvin <km638641@outlook.com>
2023-05-09 00:04:19 -0700

    create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, len = 0, written = 0;
+int fd, len = 0, written = 0;
 
-        if (!filename)
-                return (-1);
+if (!filename)
+return (-1);
 
-        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-        if (fd == -1)
-                return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
 
-        if (text_content)
-        {
-                while (text_content[len])
-                        len++;
+if (text_content)
+{
+while (text_content[len])
+len++;
 
-                written = write(fd, text_content, len);
-                if (written == -1 || written != len)
-                {
-                        close(fd);
-                        return (-1);
-                }
-        }
+written = write(fd, text_content, len);
+if (written == -1 || written != len)
+{
+close(fd);
+return (-1);
+}
+}
 
-        close(fd);
-        return (1);
+close(fd);
+return (1);
 }
